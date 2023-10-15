34e0f4b11af3e6833cdb4cbc3ad813b5f8e858ad
Rajat Das <rajat@Rajats-MacBook-Air.local>
2023-10-15 17:10:22 +0530

    BASE_INIT

---
+++ config/site.ts
@@ -0,0 +1,14 @@
+export type SiteConfig = typeof siteConfig
+
+export const siteConfig = {
+  name: "Eclair",
+  description: "Eclair is a modern eCommerce template by Rajat.",
+  footer: [
+    { name: "Home", href: "/" },
+    { name: "About", href: "#" },
+    { name: "Terms & Conditions", href: "#" },
+    { name: "Shipping & Return Policy", href: "#" },
+    { name: "Privacy Policy", href: "#" },
+    { name: "FAQ", href: "#" },
+  ],
+}
